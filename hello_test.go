97a40f78b9ee34329b9c4c8a4bda7b2874e33b6b
YashTike <yashtike.usa@gmail.com>
2024-08-13 12:06:02 -0700

    Added 2nd test for functionality, refactored test file.

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(got, want, t)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(got, want, t)
+	})
+}
+
+func assertCorrectMessage(got, want string, t testing.TB) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
