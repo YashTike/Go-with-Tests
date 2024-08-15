0fb8818a92586925cacc6dd977618439085def53
YashTike <yashtike.usa@gmail.com>
2024-08-14 22:01:33 -0700

    Added structs and methods to package structsmethods

--- structsmethods/perimeter_test.go
+++ structsmethods/perimeter_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -14,10 +15,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	got := Area(4.0, 5.0)
-	want := 20.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{4.0, 5.0}
+		got := rectangle.Area()
+		want := 20.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
