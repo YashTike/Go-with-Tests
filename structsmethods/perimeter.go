8452c551aaf93bc144e9ebfdc32327f5e8b3defc
YashTike <yashtike.usa@gmail.com>
2024-08-15 00:28:03 -0700

    Added Table Driven Test suite to TestArea, made tests more readable with clearer intent

--- structsmethods/perimeter.go
+++ structsmethods/perimeter.go
@@ -2,6 +2,10 @@ package structsmethods
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,6 +13,11 @@ type Rectangle struct {
 
 type Circle struct {
 	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
@@ -21,4 +30,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
